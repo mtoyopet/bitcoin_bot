4aa50dac6515b851c5080db8dbaa59c3b76a9fb4
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-09-27 17:53:12 +0900

    最安値を取得

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 from datetime import datetime
-time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-print(time)
+
+# ts = int("1514678400")
+# time = datetime.utcfromtimestamp(ts).strftime("%Y/%m/%d %H:%M:%S")
+# print(time)
+
+import requests
+response = requests.get("https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc?periods=86400&after=1514764800")
+data = response.json()
+
+# print(data['result']['86400'])
+
+for element in data['result']['86400']:
+  if element[0] == 1516147200:
+    print(f"最安値: {element[3]}")
+    print(f"終値: {element[4]}")
